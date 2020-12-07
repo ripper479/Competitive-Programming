9b14ce71e9626f07802fb604f3b7678f535fb142
Sourav Sharma <33860939+ripper479@users.noreply.github.com>
2020-12-07 21:27:49 +0530

    Create z_algo.cpp

---
+++ z_algo.cpp
@@ -0,0 +1,13 @@
+vector<ll> z_function(string s) {
+    ll n= sz(s);
+    vector<ll> z(n,0);
+    for (ll i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
+    }
+    return z;
+}
