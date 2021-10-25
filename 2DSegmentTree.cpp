1a96598ed99a8697622fb2dc429b93d5476a6a22
Sourav Sharma <33860939+ripper479@users.noreply.github.com>
2021-10-25 19:29:03 +0530

    Update 2DSegmentTree.cpp

--- 2DSegmentTree.cpp
+++ 2DSegmentTree.cpp
@@ -1,96 +1,165 @@
-class SegmentTree2D{
-    std::vector<std::vector<int>> st;       // to store 2D segment tree
-    std::vector<std::vector<int>> mat;      // to store matrix
-    int m, n;
+e.i/**
+ * Description: Segment Tree 2D (Support point updates and associative operations like sum, max on a 2-D integer matrix)
+ * Usage: construct O(NM),
+ *  	query O( lg(N) lg(M) ),
+ * 	update O( lg(N) lg(M) )
+ * Source: https://github.com/dragonslayerx
+ */
+class SegTree2D {
+    vector<vector<long long> > segtree;
+    size_t n, m;
+
+    bool OUT(int l, int r, int ql, int qr){
+        return (qr < l || ql > r);
+    }
+
+    bool IN(int l, int r, int ql, int qr){
+        return (ql >= l && qr <= r);
+    }
+
+    long long build_y(int node_x, int lx, int rx, int node_y, int ly, int ry, vector<vector<long long> > &array){
+        if (ly == ry) {
+            if (lx == rx) {
+                segtree[node_x][node_y] = array[lx][ly];
+            } else {
+                segtree[node_x][node_y] = operation(segtree[node_x * 2 + 1][node_y], segtree[node_x * 2 + 2][node_y]);
+            }
+            return segtree[node_x][node_y];
+        }
+        int my = (ly + ry)/2;
+        long long left = build_y(node_x, lx, rx, node_y * 2 + 1, ly, my, array);
+        long long right = build_y(node_x, lx, rx, node_y * 2 + 2, my + 1, ry, array);
+        segtree[node_x][node_y] = operation(left, right);
+        return segtree[node_x][node_y];
+    }
+
+    long long build_x(int node_x, int lx, int rx, vector<vector<long long> > &array){
+        if (lx != rx) {// demarcs that branching is still possible
+            int mx = (lx + rx)/2;
+            build_x(node_x * 2 + 1, lx, mx, array);
+            build_x(node_x * 2 + 2, mx + 1, rx, array);
+        }
+        build_y(node_x, lx, rx, 0, 0, m - 1, array);
+    }
+
+
+    long long query_y(int node_x, int node_y, int ly, int ry, int qly, int qry){
+        if (OUT(qly, qry, ly, ry)) {
+            return def;
+        } else if (IN(qly, qry, ly, ry)) {
+            return segtree[node_x][node_y];
+        }
+        int my = (ly + ry)/2;
+        long long left = query_y(node_x, node_y * 2 + 1, ly, my, qly, qry);
+        long long right = query_y(node_x, node_y * 2 + 2, my + 1, ry, qly, qry);
+        return operation(left, right);
+    }
+
+    long long query_x(int node_x, int lx, int rx, int qlx, int qrx, int qly, int qry){
+        if (OUT(qlx, qrx, lx, rx)) {
+            return def;
+        } else if (IN(qlx, qrx, lx, rx)) {
+            return query_y(node_x, 0, 0, m - 1, qly, qry);
+        }
+        int mx = (lx + rx)/2;
+        long long left = query_x(node_x * 2 + 1, lx, mx, qlx, qrx, qly, qry);
+        long long right = query_x(node_x * 2 + 2, mx + 1, rx, qlx, qrx, qly, qry);
+        return operation(left, right);
+    }
+
+    long long update_y(int node_x, int lx, int rx, int node_y, int ly, int ry, int uy, int update_value){
+        if (uy > ry || uy < ly) {
+            return segtree[node_x][node_y];
+        }
+        if (ly == ry && ly == uy) {
+            if (lx == rx) {
+                segtree[node_x][node_y] = update_value;
+            } else {
+                segtree[node_x][node_y] = operation(segtree[node_x * 2 + 1][node_y], segtree[node_x * 2 + 2][node_y]);
+            }
+            return segtree[node_x][node_y];
+        }
+        int my = (ly + ry)/2;
+        long long left = update_y(node_x, lx, rx, node_y * 2 + 1, ly, my, uy, update_value);
+        long long right = update_y(node_x, lx, rx, node_y * 2 + 2, my + 1, ry, uy, update_value);
+        segtree[node_x][node_y] = operation(left, right);
+        return segtree[node_x][node_y];
+    }
+
+    void update_x(int node_x, int lx, int rx, int ux, int uy, long long update_value){
+        if (ux > rx || ux < lx) {
+            return;
+        } if (lx == rx && lx == ux) {
+            update_y(node_x, lx, rx, 0, 0, m - 1, uy, update_value);
+            return;
+        }
+        int mx = (lx + rx)/2;
+        update_x(node_x * 2 + 1, lx, mx, ux, uy, update_value);
+        update_x(node_x * 2 + 2, mx + 1, rx, ux, uy, update_value);
+
+        update_y(node_x, lx, rx, 0, 0, m - 1, uy, update_value);
+    }
+
 
 public:
-    SegmentTree2D(std::vector<std::vector<int>> &matrix){
-        mat = matrix;
-        m = mat.size();
-        n = mat[0].size();
-        // initialize st
-        st.assign(m + m, std::vector<int> (n + n, 0));
-        build2D(1, 0, m - 1);
+    SegTree2D(int n, int m, long long def): def(def), n(n), m(m) {
+         segtree.resize(4*n, vector<long long>(4*m));
     }
 
-    void build(std::vector<int> &segTree, std::vector<int> &ar, int index, int L, int R){
-        if(L == R){
-            segTree[index] = ar[L];
-        }
-        else{
-        int mid = (L + R) / 2;
-        // 2*index will be left child
-        build(segTree, ar, 2*index, L, mid);
-        // 2*index + 1 will be right child
-        build(segTree, ar, 2*index + 1, mid + 1, R);
-        // finally, add the values from two children into the parent
-        segTree[index] = segTree[2*index] + segTree[2*index + 1];
-        }
+    SegTree2D(vector<vector<long long> > &array, int n, int m, long long def): def(def), n(n), m(m) {
+        segtree.resize(4*n, vector<long long>(4*m));
+        construct(array);
     }
 
-    int query(std::vector<int> &segTree, int index, int L, int R, int start, int end){
-        if(start > R || end < L){
-            // -1 used as null value here
-            return -1;
-        }
-
-        if (L >= start and R <= end)
-            return segTree[index];
-
-        int mid = (L + R) / 2;
-        int left = query(segTree, 2*index, L, mid, start, end);
-        int right = query(segTree, 2*index + 1, mid + 1, R, start, end);
-
-        if(left == -1)
-            return right;
-        if(right == -1)
-            return left;
-        return left + right;
+    void construct(vector<vector<long long> > &array){
+        build_x(0, 0, n - 1, array);
     }
 
-    void build2D(int index, int L, int R){
-        // if at leaf node, build a 1D segment tree
-        if(L == R)
-            build(st[index], mat[L], 1, 0, n - 1);
-    
-        else{
-            int mid = (L + R) / 2;
-            
-            // build left child segment tree
-            build2D(2*index, L, mid);
-            // build right child segment tree
-            build2D(2*index + 1, mid + 1, R);
-            
-            // merge left and right children
-            for(int i = 0; i < st[index].size(); ++i)
-                st[index][i] = st[2*index][i] + st[2*index + 1][i];
-        }
+    long long query(int qlx, int qrx, int qly, int qry){
+        return query_x(0, 0, n - 1, qlx, qrx, qly, qry);
     }
 
-    int query2D(int index, int L, int R, int x1, int y1, int x2, int y2){
-        if(L > x2 || R < x1)
-            return -1;
-
-        if(L >= x1 and R <= x2)
-            return query(st[index], 1, 0, n - 1, y1, y2);
-
-        int mid = (L + R) / 2;
-
-        // query left child. Returned value will be integer
-        int left = query2D(2*index, L, mid, x1, y1, x2, y2);
-        // query right child. Returned value will be integer
-        int right = query2D(2*index + 1, mid + 1, R, x1, y1, x2, y2);
-
-        if(left == -1)
-            return right;
-        if(right == -1)
-            return left;
-        
-        return left + right;
+    void update(int ux, int uy, long long update_value){
+        update_x(0, 0, n - 1, ux, uy, update_value);
     }
 
-    // Main query function
-    int query(int x1, int y1, int x2, int y2){
-        return query2D(1, 0, m - 1, x1, y1, x2, y2);
+#ifdef DEBUG
+ void print(){
+    for (int i = 0; i < 4*n; i++) {
+        for (int j = 0; j < 4*m; j++)
+            printf("(%d, %d)", segtree[i][j].maxx, segtree[i][j].count);
+        cout << endl;
+    }
+ }
+#endif
+
+private:
+    long long operation(long long left, long long right){
+        return max(left, right);
     }
 };
+
+int main()
+{
+    int tc;
+    scanf("%d", &tc);
+    for (int t=1; t<=tc; t++) {
+        printf("Case %d:\n", t);
+        int n, q;
+        scanf("%d%d", &n, &q);
+        vector<vector<int> > array(n, vector<int> (n));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                scanf("%d", &array[i][j]);
+            }
+        }
+        SegTree2D<int> S(array, n, n, 0);
+        while (q--) {
+            int x, y, s;
+            scanf("%d%d%d", &x, &y, &s);
+            x--, y--;
+            //cout << x << " " << y << " " << x+s-1 << " " << y+s-1 << endl;
+            printf("%d\n", S.query(x, x+s-1, y, y+s-1));
+        }
+    }
+}
